165d6b3fc165c5ed21c70a489441723acfae48e8
Daniel Fanara <fanardat16@gmail.com>
2016-01-13 10:55:10 -0500

    Initial Commit

---
+++ src/main/java/com/shdwlf/safetydepositbox/SafetyDepositBox.java
@@ -0,0 +1,17 @@
+package com.shdwlf.safetydepositbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SafetyDepositBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
